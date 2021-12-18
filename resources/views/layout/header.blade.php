0f206fb0425952a01ede0aa652ef2041c33194be
Zahidul-Islam94 <72647292+Zahidul-Islam94@users.noreply.github.com>
2021-12-18 10:11:54 +0600

    git commit
    Vendor folder added

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,10 +1,10 @@
-<nav class="navbar navbar-expand-lg text-white navbar-light bg-primary">
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
           <a class="nav-link" href="{{ url('skills') }}">Skills</a>
@@ -17,3 +17,9 @@
       </div>
     </div>
   </nav>
+
+  <style>
+    a{
+      color: white!important;
+    }
+  </style>
