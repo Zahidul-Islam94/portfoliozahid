a47966b4bae15729fe9f6b632a01873720f531fc
Zahidul-Islam94 <72647292+Zahidul-Islam94@users.noreply.github.com>
2021-12-20 14:10:49 +0600

    vendor folder added

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg text-white navbar-light bg-primary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#"><img src="{{ asset('/photos/logo.png/') }}" alt="image"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
